80bbaa22b6380b90617c1154ead4e35a9ebac026
DanielRicra <daniel.int@outlook.com>
2024-03-13 20:07:54 -0500

    Update api.service.ts
    Add the post, put and delete methods
    
    Add baseUrl property to use in all urls

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,15 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Options } from '../../types';
+import { Options, Product } from '../../types';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
+  private readonly baseUrl = 'http://localhost:3000/api';
   constructor(private httpClient: HttpClient) {}
 
   get<T>(url: string, options: Options): Observable<T> {
-    return this.httpClient.get<T>(url, options);
+    return this.httpClient.get<T>(this.baseUrl + url, options);
+  }
+
+  post<T>(url: string, body: Product, options: Options): Observable<T> {
+    return this.httpClient.post<T>(this.baseUrl + url, body, options);
+  }
+
+  put<T>(url: string, body: Product, options: Options): Observable<T> {
+    return this.httpClient.put<T>(this.baseUrl + url, body, options);
+  }
+
+  delete<T>(url: string, options: Options): Observable<T> {
+    return this.httpClient.delete<T>(this.baseUrl + url, options);
   }
 }
